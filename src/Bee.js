bc313e455dcba2616591146399d23f28d17c3645
Ingrid Carlson <ingridclaire@gmail.com>
2020-03-31 08:28:15 -0700

    Implemented all the bees in ES6!

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
-class Bee {
-  // TODO..
-};
+class Bee extends Grub {
+  constructor() {
+    super()
+    this.age = 5
+    this.color = 'yellow'
+    this.job = 'Keep on growing'
+  }
+}
+
+/*he Grub superclass
+an age property that is set to 5
+a color property that is set to yellow
+a food property that is inherited from grub
+an eat method that is inherited from grub
+a job property that is set to Keep on growing*/
